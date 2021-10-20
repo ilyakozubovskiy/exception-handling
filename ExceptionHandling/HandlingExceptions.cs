8ab8d751bc77729b22ffe84f393883ca7815d49a
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-10-20 21:55:07 +0300

    exception-handling

--- ExceptionHandling/HandlingExceptions.cs
+++ ExceptionHandling/HandlingExceptions.cs
@@ -1,3 +1,5 @@
+﻿#pragma warning disable CA2201
+#pragma warning disable S112
 using System;
 
 namespace ExceptionHandling
@@ -6,8 +8,14 @@ namespace ExceptionHandling
     {
         public static bool CatchException(object obj)
         {
-            // TODO #1. Add a try-catch construction here to catch all exceptions. The method should return true if an exception is thrown; otherwise false.
-            ThrowException(obj);
+            try
+            {
+                ThrowException(obj);
+            }
+            catch (Exception)
+            {
+                return true;
+            }
 
             return false;
         }
@@ -15,9 +23,15 @@ namespace ExceptionHandling
         public static bool CatchArgumentNullException(object obj, out string exceptionMessage)
         {
             exceptionMessage = string.Empty;
-
-            // TODO #2. Add a try-catch construction here to catch "ArgumentNullException". The method should return true if an "ArgumentNullException" is thrown and set "exceptionMessage" parameter to an exception message; otherwise the method should return false.
-            ThrowException(obj);
+            try
+            {
+                ThrowException(obj);
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
 
             return false;
         }
@@ -25,9 +39,15 @@ namespace ExceptionHandling
         public static bool CatchArgumentException(int i, out string exceptionMessage)
         {
             exceptionMessage = string.Empty;
-
-            // TODO #3. Add a try-catch construction here to catch "ArgumentException". The method should return true if an "ArgumentException" is thrown and set "exceptionMessage" parameter to an exception message; otherwise the method should return false.
-            ThrowException(new object(), i);
+            try
+            {
+                ThrowException(new object(), i);
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
 
             return false;
         }
@@ -35,9 +55,15 @@ namespace ExceptionHandling
         public static bool CatchArgumentOutOfRangeException(int j, out string exceptionMessage)
         {
             exceptionMessage = string.Empty;
-
-            // TODO #4. Add a try-catch construction here to catch "ArgumentOutOfRangeException". The method should return true if an "ArgumentOutOfRangeException" is thrown and set "exceptionMessage" parameter to an exception message; otherwise the method should return false.
-            ThrowException(new object(), 1, j);
+            try
+            {
+                ThrowException(new object(), 1, j);
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
 
             return false;
         }
@@ -45,14 +71,30 @@ namespace ExceptionHandling
         public static bool CatchExceptions(object obj, int i, int j, bool throwException, out string exceptionMessage)
         {
             exceptionMessage = string.Empty;
-
-            // TODO #5. Add a try-catch construction here to catch exceptions with these types:
-            // * Exception
-            // * ArgumentException
-            // * ArgumentNullException
-            // * ArgumentOutOfRangeException
-            // The method should return true if an exception of any type is thrown and set "exceptionMessage" parameter to an exception message; otherwise the method should return false.
-            ThrowException(obj, i, j, throwException);
+            try
+            {
+                ThrowException(obj, i, j, throwException);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (ArgumentNullException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (ArgumentException ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = ex.Message;
+                return true;
+            }
 
             return false;
         }
